079c6910d10f1fd6a5044b9d99b5f2cb2eaf8fd8
Auliakartika150 <auliakartika150@gmail.com>
2025-07-17 22:05:08 +0700

    Update CORS config

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,24 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
-    'allowed_methods' => ['*'],
-    
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
-    'allowed_origins_patterns' => [
-        '^https:\/\/.*\.vercel\.app$',
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://web-bps-frontend.vercel.app',
     ],
 
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true, 
+
+    'supports_credentials' => true,
+
 ];
