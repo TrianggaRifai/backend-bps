1f40281783ff5c6e5ddcfd09194de82cc06e8f5e
Triangga Rifa'i <trianggarifai@MacBook-Air-Triangga.local>
2025-08-01 01:26:16 +0700

    Fix: remove duplicate migration file

--- config/cors.php
+++ config/cors.php
@@ -1,32 +1,23 @@
 <?php
 
 return [
-<<<<<<< HEAD
-=======
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
     
+    // Gunakan environment variable untuk URL frontend
     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
->>>>>>> 8eb7c7efae6329de0c937d51391335d44daa5b58
-
-    'paths' => ['api/*'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-<<<<<<< HEAD
 
+    // Pastikan ini bernilai true
     'supports_credentials' => true,
-
-=======
-    'supports_credentials' => true, 
->>>>>>> 8eb7c7efae6329de0c937d51391335d44daa5b58
 ];
